ea2799b04d65255609a0da5903beb1db5c3f6407
wja <2842698953@qq.com>
2025-04-10 17:58:06 +0800

    sort

---
+++ build/CMakeFiles/algorithm.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/algorithm.dir/Sort/question1.cpp.o"
+  "CMakeFiles/algorithm.dir/Sort/question1.cpp.o.d"
+  "algorithm"
+  "algorithm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/algorithm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
