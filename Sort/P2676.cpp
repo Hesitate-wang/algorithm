4e3bf9577a4b7c9ef93b21437f89641feabb9d17
wja <2842698953@qq.com>
2025-04-10 17:57:17 +0800

    sort

---
+++ Sort/P2676.cpp
@@ -0,0 +1,25 @@
+
+
+
+struct Heap{
+    vector<long long> cows;
+    Heap(){
+        cows = vector<long long>();
+    }
+    void push(long long cow){
+        cows.push_back(cow);
+        upSift();
+    }
+    void upSift()
+}
+
+
+
+
+
+int main(){
+    int N;
+    long long B;
+    cin >> N >> B;
+
+}
